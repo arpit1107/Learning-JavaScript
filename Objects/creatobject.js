aa9a1856f906d25037dacf76957d8299de8bf431
arpit katiyar <arpitkatiyar1107@gmail.com>
2021-02-26 20:15:42 +0530

    26-02-21

---
+++ Objects/creatobject.js
@@ -0,0 +1,16 @@
+//In this file learn create the object
+//this way create the object => {}
+//in js "{}" is the object
+
+var object= {
+    //property: "value"
+    name: 'arpit',
+    sername: 'katiyar',
+    Degree: 'Btech',
+    Branch: 'Computer Science and engineering'
+};
+
+console.log("Name => ", object.name);
+console.log("Sername =>", object.sername);
+console.log("Degree =>", object.Degree);
+console.log("Branch =>",object.Branch);
